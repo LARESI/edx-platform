718197f06c3cc2a945f489cbc3d7c62c57223a6a
usama sadiq <usama@A003-00183.arbisoft.com>
2019-05-03 12:23:28 +0500

    MML-AM_CHTML configuration

--- common/test/db_cache/bok_choy_migrations_data_default.sql
+++ common/test/db_cache/bok_choy_migrations_data_default.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.32, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
 --
--- Host: localhost    Database: edxtest
+-- Host: edx.devstack.mysql    Database: edxtest
 -- ------------------------------------------------------
--- Server version	5.6.32-1+deb.sury.org~xenial+0.1
+-- Server version	5.6.44
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,4 +34,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-25 14:14:31
+-- Dump completed on 2019-04-29  9:59:44
--- common/test/db_cache/bok_choy_migrations_data_student_module_history.sql
+++ common/test/db_cache/bok_choy_migrations_data_student_module_history.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.32, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
 --
--- Host: localhost    Database: student_module_history_test
+-- Host: edx.devstack.mysql    Database: student_module_history_test
 -- ------------------------------------------------------
--- Server version	5.6.32-1+deb.sury.org~xenial+0.1
+-- Server version	5.6.44
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,4 +34,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-25 14:15:04
+-- Dump completed on 2019-04-29 10:00:11
